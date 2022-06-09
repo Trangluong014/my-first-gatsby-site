9d10bcfce2c15268c048390e77932103de465cef
Trang Luong <luongtrang014@gmail.com>
2022-06-09 23:09:09 +0800

    update

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -16,19 +16,11 @@ const BlogPost = ({ data }) => {
 export const query = graphql`
   query ($id: String) {
     mdx(id: { eq: $id }) {
-      body
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
-        hero_image_alt
-        hero_image_credit_link
-        hero_image_credit_text
-        hero_image {
-          childImageSharp {
-            gatsbyImageData
-          }
-        }
+        date(formatString: "MMMM D, YYYY")
       }
+      body
     }
   }
 `;
